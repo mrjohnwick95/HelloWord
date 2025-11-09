30ce24bf57609176fb443c2662b9cf1a13838ed8
Salome <rodrimartha18@gmail.com>
2025-11-09 16:04:50 -0500

    add acount dockerHub

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const path = require('path'); 
 const app = express();
 
-// Las plataformas de nube asignan un puerto dinámicamente.
-// Usamos process.env.PORT para tomar ese puerto, o el 3000 si es local.
 const PORT = process.env.PORT || 3000;
 
+
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
-  res.send('¡Hola Mundo! Este proyecto está desplegado.');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
